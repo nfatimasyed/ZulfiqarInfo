450058e8cb27c2c78df199a35be61b99a3212b4c
nfatimasyed <38048800+nfatimasyed@users.noreply.github.com>
2023-05-11 08:40:46 +0100

    Add files via upload

---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class btn_hello : Form
+    {
+        public btn_hello()
+        {
+            InitializeComponent();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Learning WinForms Application  : ", "Error ");
+        }
+    }
+}
